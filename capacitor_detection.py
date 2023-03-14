77aa412e5b168048308d03f28d132ae6e0fad204
Mikel <mikelgalafate15@gmail.com>
2023-03-14 12:49:28 +0100

    Create functions #5

--- capacitor_detection.py
+++ capacitor_detection.py
@@ -1,14 +1,43 @@
 import argparse
+import json
+import os
 import cv2
 import numpy as np
 
 
-def helloworld():
+def validate_board(im_path, loc_path):
     """
-    Hace algo
+
+    :param im_path:
+    :param loc_path:
     :return:
     """
-    return "hello world"
+    segments = seg_image(im_path)
+    capacitors = loc_capacitors(segments)
+    big, small = extract_types(capacitors)
+
+    if len(big) * 0.15 + len(small) * 0.05 > 1:
+        if not os.path.exists(loc_path):
+            os.mkdir(loc_path)
+        im_name = im_path.split(".")[0]
+        f = open(loc_path + "/" + im_name + "_loc.txt", "w")
+        json.dump((big, small), f)
+        return True
+    else:
+        return False
+
+
+def extract_types(capacitors):
+    big, small = ([[], [], [], [], [], []], [[], [], []])
+    return big, small
+
+
+def loc_capacitors(segments):
+    return []
+
+
+def seg_image(im_path):
+    return []
 
 
 if __name__ == '__main__':
@@ -19,4 +48,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-
+    validate_board(args.img, args.loc)
--- capacitor_detection_test.py
+++ capacitor_detection_test.py
@@ -1,10 +1,21 @@
+import json
 import unittest
 import capacitor_detection as cd
 
 
-class Testing(unittest.TestCase):
-    def test_helloworld(self):
+class CapacitorDetectionTest(unittest.TestCase):
+    def test_validate_board(self):
         """
-        Realiza pruebas sobre la función hello world
+        Comprueba que una placa evaluada como positiva ha sido bien validada
+        :return:
         """
-        self.assertEqual(cd.helloworld(), "hello world")
+        cd.validate_board("test.jpg", "./locations")
+        try:
+            with open("./locations/test_loc.txt", "r") as f:
+                big, small = json.load(f)
+                self.assertGreater(len(big) * 0.15 + len(small) * 0.05, 1, "Placa mal validada")
+        except OSError as e:
+            return
+
+    def test_validate_board_out(self):
+        self.assertIsInstance(cd.validate_board("test.jpg", "./locations"), bool)
