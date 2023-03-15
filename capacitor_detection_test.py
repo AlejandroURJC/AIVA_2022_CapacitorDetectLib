b32c3dc5187c85cf2f1ce9ab9571de78f826ab21
Mikel <mikelgalafate15@gmail.com>
2023-03-15 11:18:38 +0100

    Create new functions and tests #5
    New functions for the mock-up and some tests were created

--- capacitor_detection_test.py
+++ capacitor_detection_test.py
@@ -2,20 +2,43 @@ import json
 import unittest
 import capacitor_detection as cd
 
+testim = "./DB/rec1-1.jpg"
+testloc = "./locations"
+
 
 class CapacitorDetectionTest(unittest.TestCase):
+    def test_read_txt(self):
+        """
+        Comprueba que la función read_txt lee correctamente el fichero
+        :return:
+        """
+        big, small = cd.read_txt(testim, testloc)
+        self.assertIsNotNone(big, "Archivo no leído correctamente")
+        self.assertIsNotNone(small, "Archivo no leído correctamente")
+
+    def test_write_txt(self):
+        """
+        Comprueba que la función write_txt escribe correctamente en el fichero
+        :return:
+        """
+        big, small = ([], [])
+        cd.write_txt((big, small), testim, testloc)
+        new_big, new_small = cd.read_txt(testim, testloc)
+        self.assertEqual(big, new_big, "Lo escrito y lo leído no coinciden")
+        self.assertEqual(small, new_small, "Lo escrito y lo leído no coinciden")
+
     def test_validate_board(self):
         """
         Comprueba que una placa evaluada como positiva ha sido bien validada
         :return:
         """
-        cd.validate_board("test.jpg", "./locations")
-        try:
-            with open("./locations/test_loc.txt", "r") as f:
-                big, small = json.load(f)
-                self.assertGreater(len(big) * 0.15 + len(small) * 0.05, 1, "Placa mal validada")
-        except OSError as e:
-            return
+        self.assertTrue(cd.validate_board(testim, testloc), "Placa no válida")
+        big, small = cd.read_txt(testim, testloc)
+        self.assertGreater(len(big) * 0.15 + len(small) * 0.05, 1, "Placa mal validada")
 
-    def test_validate_board_out(self):
-        self.assertIsInstance(cd.validate_board("test.jpg", "./locations"), bool)
+    def test_read_image(self):
+        """
+        Comprueba que la imagen se lee correctamente con el tamaño debido
+        :return:
+        """
+        self.assertEqual(cd.read_image(testim).shape, (3280, 4928))
